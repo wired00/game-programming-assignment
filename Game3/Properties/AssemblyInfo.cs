06fdf7cfd142d4a8264cb5cb971b205a16272ed1
KyronHo <KyronCHo@gmail.com>
2015-09-12 21:22:59 +1000

    Added icon and renamed title

--- Game3/Properties/AssemblyInfo.cs
+++ Game3/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Game3")]
-[assembly: AssemblyProduct("Game3")]
+[assembly: AssemblyTitle("Battery Derby")]
+[assembly: AssemblyProduct("Battery Derby")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Group21")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
